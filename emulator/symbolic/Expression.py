0273dfd70264b9ffcf453ae0a91cbfc9ea0cab42
Pablo Sole <pablo@remotium.com>
2013-07-21 02:29:56 -0300

    initial Expression list

---
+++ emulator/symbolic/Expression.py
@@ -0,0 +1,45 @@
+'''
+Created on Jul 21, 2013
+
+@author: pablo
+
+Types
+=====
+
+Bitwise
+    AND
+    OR
+    NOT
+    XOR
+
+Shifts
+    SHL
+    SHR
+
+Arithmetic
+    ADD
+    SUB
+    MUL
+    UDIV
+    UREM
+    NEG
+
+Ordering
+    ULT
+    ULE
+    EQ
+    NEQ
+    UGT
+    UGE
+    SLT
+    SLE
+    SGT
+    SGE
+
+Miscellaneous
+    CONCAT
+    EXTRACT
+    ITE
+    ZEROEXTEND
+    SIGNEXTEND
+'''
