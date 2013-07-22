f523f7ae8cc6d8ffe881ad5a87524b503766f80e
Pablo Sole <pablo@remotium.com>
2013-07-21 22:37:24 -0300

    more info about Expressions

--- emulator/symbolic/Expression.py
+++ emulator/symbolic/Expression.py
@@ -3,9 +3,6 @@ Created on Jul 21, 2013
 
 @author: pablo
 
-Types
-=====
-
 Bitwise
     AND
     OR
@@ -46,6 +43,31 @@ Miscellaneous
 Constants
     FALSE
     TRUE
+    BVCONST
+
+Variables
+    BOOL
     BV
 
+Expressions
+    BoolExpr
+        have pythonic methods for all typical boolean operations
+    BVExpr
+        have pythonic method for BV operations.
+        it never adapts the size of the expression by itself
+    
+    Once created, the expression should be considered immutable.
+    __init__ functions should be avoided as much as possible.
+    All type information should be static and part of the class definition.
 '''
+
+class Expr:
+    def __str__(self):
+        pass
+
+class BoolExpr(Expr):
+    __type__="boolean"
+
+class BVExpr(Expr):
+    __type__="bitvector"
+    
