c75b3cf6ece1c56125989703233d36f18684229f
Pablo Sole <pablo@remotium.com>
2013-07-25 14:41:35 -0300

    Modified MemoryMap to make it more suitable for an Abstract memory map and added support for switching between big and little endian

--- emulator/memory.py
+++ emulator/memory.py
@@ -3,11 +3,22 @@ Created on Jun 12, 2013
 
 @author: anon
 '''
-import struct
 
 class MemoryMap(object):
-    def __init__(self):
-        pass
+    LittleEndianMode=0
+    BigEndianMode=1
+
+    def __init__(self, mode=LittleEndianMode):
+        self.mode = mode
+        self.setMode(mode)
+    
+    def setMode(self, mode):
+        if mode == MemoryMap.LittleEndianMode:
+            self.__split_bytes__ = self.__split_bytes_le__
+            self.__join_bytes__ = self.__join_bytes_le__
+        else:
+            self.__split_bytes__ = self.__split_bytes_be__
+            self.__join_bytes__ = self.__join_bytes_be__
         
     def __getitem__(self, address):
         raise Exception("Method not implemented.")
@@ -15,29 +26,61 @@ class MemoryMap(object):
     def __setitem__(self, address, value):
         raise Exception("Method not implemented.")
     
-    def __set_bytes__(self, address, value, size):
+    def __set_bytes__(self, address, value):
+        """
+        Receives an iterable of byte values.
+        All byte values are already inside the boundaries of a 8-bits byte.
+        """
         raise Exception("Method not implemented.")
     
     def __get_bytes__(self, address, size):
         raise Exception("Method not implemented.")
     
+    def __split_bytes_le__(self, value, size):
+        raise Exception("Method not implemented.")
+
+    def __join_bytes_le__(self, value, size):
+        raise Exception("Method not implemented.")
+
+    def __split_bytes_be__(self, value, size):
+        raise Exception("Method not implemented.")
+
+    def __join_bytes_be__(self, value, size):
+        raise Exception("Method not implemented.")
+    
     def set_byte(self, address, value):
-        self.__set_bytes__(address, struct.pack("<B", value & 0xff), 1)
+        self.__set_bytes__(address, self.__split_bytes__(value, 1))
     
     def set_word(self, address, value):
-        self.__set_bytes__(address, struct.pack("<H", value & 0xffff), 2)
+        self.__set_bytes__(address, self.__split_bytes__(value, 2))
     
     def set_dword(self, address, value):
-        self.__set_bytes__(address, struct.pack("<L", value & 0xffffffff), 4)
+        self.__set_bytes__(address, self.__split_bytes__(value, 4))
+
+    def set_qword(self, address, value):
+        self.__set_bytes__(address, self.__split_bytes__(value, 8))
+    
+    def set_bytes(self, address, chars):
+        for i in range(len(chars)):
+            self.set_byte(address + i, ord(chars[i]))
 
     def get_byte(self, address):
-        return self.__get_bytes__(address, 1)
+        return self.__join_bytes__(self.__get_bytes__(address, 1))
     
     def get_word(self, address):
-        return self.__get_bytes__(address, 2)
+        return self.__join_bytes__(self.__get_bytes__(address, 2))
     
     def get_dword(self, address):
-        return self.__get_bytes__(address, 4)
+        return self.__join_bytes__(self.__get_bytes__(address, 4))
+    
+    def get_qword(self, address):
+        return self.__join_bytes__(self.__get_bytes__(address, 8))
+    
+    def get_bytes(self, address, size):
+        out=""
+        for ch in self.__get_bytes__(address, size):
+            out += chr(ch)
+        return out
 
 class ConcreteMemoryMap(MemoryMap):
     def __init__(self):
@@ -50,22 +93,40 @@ class ConcreteMemoryMap(MemoryMap):
     def __getitem__(self, address):
         return self.memory[address]
 
-    def __set_bytes__(self, address, value, size):
-        for i in xrange(0, size):
-            #print "[%.8x]  = %.2x" % (address + i, ord(value[i]))
-            self.__setitem__(address + i, ord(value[i]))
+    def __set_bytes__(self, address, value):
+        for i in range(len(value)):
+            #print "[%.8x]  = %.2x" % (address + i, value[i])
+            self.__setitem__(address + i, value[i])
     
     def __get_bytes__(self, address, size):
         values = []
         for i in xrange(0, size):
             values.append(self.__getitem__(address + i))
         
-        # Concatenate the bytes
+        return values
+    
+    def __split_bytes_le__(self, value, size):
+        out=[]
+        for _ in range(size):
+            out.append(value & 0xff)
+            value >>= 8
+        return out
+    
+    def __join_bytes_le__(self, values):
         val = values[0]
-        for x in xrange(1, size):
-            val |= values[x] << (8 * x)        
-        
+        for x in range(1, len(values)):
+            val |= values[x] << (8 * x)
         return val
+    
+    def __split_bytes_be__(self, value, size):
+        out=self.__split_bytes_le__(value, size)
+        out.reverse()
+        return out
+
+    def __join_bytes_be__(self, values):
+        values.reverse()
+        return self.__join_bytes_le__(values)
+        
 
 class DummyMemoryMap(ConcreteMemoryMap):
     """
--- test_emulator.py
+++ test_emulator.py
@@ -1,5 +1,5 @@
 from emulator.ARMEmulator import ARMEmulator
-from emulator.memory import ConcreteMemoryMap
+from emulator.memory import ConcreteMemoryMap, MemoryMap
 import logging
 
 def main():
@@ -10,7 +10,7 @@ def main():
 
     # Fill the memory map with instructions at memory address 0xcafe0000
     ins = "\x00\x00\xa0\xe3\x01\x10\xa0\xe3\x02\x20\xa0\xe3\x03\x30\xa0\xe3\x04\x40\xa0\xe3\x05\x50\xa0\xe3\x06\x60\xa0\xe3\x07\x70\xa0\xe3\x08\x80\xa0\xe3\x09\x90\xa0\xe3\x01\x00\x80\xe2\x01\x10\x81\xe2\x01\x20\x82\xe2\x01\x30\x83\xe2\x01\x40\x84\xe2\x01\x50\x85\xe2\x01\x60\x86\xe2\x01\x70\x87\xe2\x01\x80\x88\xe2\x01\x90\x89\xe2"
-    memory_map.__set_bytes__(0xcafe0000, ins, len(ins))
+    memory_map.set_bytes(0xcafe0000, ins)
     
     emulator = ARMEmulator(memory_map)
     emulator.setPC(0xcafe0000)
@@ -19,6 +19,11 @@ def main():
     emulator.step()
     emulator.step()
     emulator.step()
+    
+    memory_map.set_dword(0xcafecafe, 0xdeadcafe)
+    print "%x" % memory_map.get_dword(0xcafecafe)
+    memory_map.setMode(MemoryMap.BigEndianMode)
+    print "%x" % memory_map.get_dword(0xcafecafe)
 
 if __name__ == '__main__':
     main()
